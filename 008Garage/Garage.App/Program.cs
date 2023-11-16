aaa60e7e6b8cea5553bccabc233dfb7afab578e2
Simon Koller <113828769+shotgun007@users.noreply.github.com>
2023-11-16 20:59:56 +0100

    Add files via upload

---
+++ 008Garage/Garage.App/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.ComponentModel.Design;
+using Garage.Logic;
+
+GarageClass g;
+g = new GarageClass();
+string selection;
+int parkingSpotNumber;
+decimal costs;
+do
+{
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("1) Enter a car entry");
+    Console.WriteLine("2) Enter a car exit");
+    Console.WriteLine("3) Generate report");
+    Console.WriteLine("4) Exit");
+    selection = Console.ReadLine()!;
+    Console.WriteLine($"Your selection: {selection}");
+
+    switch (selection)
+    {
+        case "1":
+            Console.Write("Enter parking spot number: ");
+            parkingSpotNumber = int.Parse(Console.ReadLine()!);
+            Console.Write("Enter license plate: ");
+            string licensePlate = Console.ReadLine()!;
+            Console.Write("Enter entry date/time: ");
+            DateTime entryTime = DateTime.Parse(Console.ReadLine()!);
+            g.TryOccupy(parkingSpotNumber, licensePlate, entryTime);
+            break;
+        case "2":
+            Console.Write("Enter parking spot number: ");
+            parkingSpotNumber = int.Parse(Console.ReadLine()!);
+            Console.Write("Enter exit date/time: ");
+            DateTime exitTime = DateTime.Parse(Console.ReadLine()!);
+            g.TryExit(parkingSpotNumber, exitTime,out costs);
+            break;
+        case "3":
+            g.A();
+            break;
+        case "4":
+            Console.WriteLine("Good Bye!");
+            break;
+        default:
+            Console.WriteLine("Invalid input");
+            break;
+    }
+
+
+
+
+}
+while (selection != "4");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 008Garage/Garage.Logic/Class1.cs
@@ -0,0 +1,108 @@
+﻿namespace Garage.Logic;
+public class ParkingSpot
+{
+    public ParkingSpot(string licensePlate, DateTime entryTime)
+    {
+        LicensePlate = licensePlate;
+        EntryTime = entryTime;
+    }
+    public string LicensePlate { get; set; }
+    public DateTime EntryTime { get; set; }
+}
+public class GarageClass
+{
+    public bool IsOccupied(int parkingSpotNumber)
+    {
+        if (ParkingSpots[parkingSpotNumber - 1] is not null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool TryOccupy(int parkingSpotNumber, string licensePlate, DateTime entryTime)
+    {
+        if (IsOccupied(parkingSpotNumber))
+        {
+            Console.WriteLine("Parking spot is occupied");
+            return false;
+        }
+        else
+        {
+
+            ParkingSpots[parkingSpotNumber - 1] = new ParkingSpot(licensePlate, entryTime);
+            return true;
+        }
+    }
+    public bool TryExit(int parkingSpotNumber, DateTime exitTime, out decimal costs)
+    {
+        if (IsOccupied(parkingSpotNumber))
+        {
+            var time = exitTime - ParkingSpots[parkingSpotNumber - 1].EntryTime;
+            int halfHour = time.Minutes / 30;
+            if (time.Minutes % 30 != 0)
+            {
+                halfHour++;
+            }
+            if (time.Minutes > 15)
+            {
+                costs = halfHour * 3;
+            }
+            else
+            {
+                costs = 0;
+            }
+            ParkingSpots[parkingSpotNumber - 1] = null!;
+            Console.WriteLine($"Costs are {costs}");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+            costs = 0;
+            return false;
+        }
+    }
+    public void A()
+    {
+        Console.WriteLine("| Spot | License Plate |");
+        for (int i = 0; i < ParkingSpots.Length; i++)
+        {
+            Console.Write($"| {i + 1}");
+            if (i < 9)
+            {
+                Console.Write("    | ");
+            }
+            else
+            {
+                Console.Write("   | ");
+            }
+            if (ParkingSpots[i] != null)
+            {
+                Console.Write(ParkingSpots[i].LicensePlate);
+                for (int j = 14 - ParkingSpots[i].LicensePlate.Length; j > 0; j--)
+            {
+                Console.Write(" ");
+            }
+            }
+            else
+            {
+                Console.Write("              ");
+            }
+            
+            Console.WriteLine("|");
+        }
+    }
+    public ParkingSpot[] ParkingSpots { get; } = new ParkingSpot[50];
+}
+
+
+
+
+
+
+
+
+
